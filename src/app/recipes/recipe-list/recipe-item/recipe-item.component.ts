21bb117667d24bb42d286670577e8059658c575c
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-06-17 11:13:53 +0100

    event emitter and click function created

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+// creates a function which has been called when the link is clicked in the html
+// creates any properties which are being used in the function (event emitter)
+//the function will emit an event which is listened for in the recipe-detail component html.
+
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -10,9 +14,23 @@ export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
 
+  @Output() recipeNameSent = new EventEmitter<any>();
+  @Output() recipeDescriptionSent = new EventEmitter<any>();
+
+  recipeName: string;
+  recipeDescription: string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected() {
+    this.recipeName = this.recipe.name;
+    this.recipeDescription = this.recipe.description;
+    // this function needs to send the data to the recipe detail component and emit an event for the recipe detail component to listen for
+    this.recipeNameSent.emit(this.recipeName);
+    this.recipeDescriptionSent.emit(this.recipeDescription);
   }
 
 }
