2985809d8285b909bae025fcd381fa6e9604013f
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-07-26 15:02:26 +0100

    now displaying ingredients when a recipe is clicked on

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
@@ -10,8 +11,26 @@ export class RecipeService {
   }
 
   private recipes: Recipe[] = [
-    new Recipe(1, "Beans on Toast", "A tin of baked beans on a slice of toast", "https://thumbs.dreamstime.com/b/beans-toast-9341313.jpg"),
-    new Recipe(2, "Cheese on Toast", "A slice of cheese on a slice of toast", "https://thumbs.dreamstime.com/b/cheese-toast-9705535.jpg"),
+    new Recipe(
+      1,
+      "Beans on Toast",
+      "A tin of baked beans on a slice of toast",
+      "https://thumbs.dreamstime.com/b/beans-toast-9341313.jpg",
+      [
+        new Ingredient("tin of baked beans", 1),
+        new Ingredient("slice of bread", 1),
+        new Ingredient("butter", 1)
+      ]),
+    new Recipe(
+      2,
+      "Cheese on Toast",
+      "A slice of cheese on a slice of toast",
+      "https://thumbs.dreamstime.com/b/cheese-toast-9705535.jpg",
+      [
+        new Ingredient("slice of cheese", 1),
+        new Ingredient("slice of bread", 1),
+        new Ingredient("butter", 1)
+      ]),
 
   ];
 
