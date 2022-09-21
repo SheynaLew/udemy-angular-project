4d6fe36304168d84989195f9841be9f1bcc70445
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-09-21 16:37:10 +0100

    160 child routes challenge - made a no recipe component and used child routing for it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { NoRecipeComponent } from "./recipes/no-recipe/no-recipe.component";
 import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
@@ -8,7 +9,8 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
     path: 'recipes', component: RecipesComponent, children: [
-      { path: ':id', component: RecipeItemComponent }
+      { path: ':id', component: RecipeItemComponent },
+      { path: 'no-recipe', component: NoRecipeComponent }
     ]
   },
   { path: 'shopping-list', component: ShoppingListComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './services/shopping-list.service';
 import { RecipeService } from './services/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
+import { NoRecipeComponent } from './recipes/no-recipe/no-recipe.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { AppRoutingModule } from './app-routing.module';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    NoRecipeComponent
   ],
   imports: [
     BrowserModule,
