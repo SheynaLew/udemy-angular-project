418765b762614c8f01fc4acf4c3a53324b275c6a
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-09-21 16:28:53 +0100

    160 child routes challenge - got child route working for recipes in list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      { path: ':id', component: RecipeItemComponent }
+    ]
+  },
   { path: 'shopping-list', component: ShoppingListComponent }
 ]
 @NgModule({
