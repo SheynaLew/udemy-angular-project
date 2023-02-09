3badce8c0733028e4ecd65af0ed7038efba1c233
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2023-02-09 16:17:18 +0000

    282 - Transforming response data

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -9,8 +9,9 @@ import { ShoppingListService } from "./shopping-list.service";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  constructor(private shoppingListService: ShoppingListService) { };
+  private recipes: Recipe[] = [];
 
+  constructor(private shoppingListService: ShoppingListService) { };
 
   get immutableRecipes(): Recipe[] {
     return this.recipes;
@@ -19,30 +20,6 @@ export class RecipeService {
   getRecipe(index: number) {
     return this.recipes[index];
   }
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      1,
-      "Beans on Toast",
-      "A tin of baked beans on a slice of toast",
-      "https://thumbs.dreamstime.com/b/beans-toast-9341313.jpg",
-      [
-        new Ingredient("tin of baked beans", 1),
-        new Ingredient("slice of bread", 1),
-        new Ingredient("butter", 1)
-      ]),
-    new Recipe(
-      2,
-      "Cheese on Toast",
-      "A slice of cheese on a slice of toast",
-      "https://thumbs.dreamstime.com/b/cheese-toast-9705535.jpg",
-      [
-        new Ingredient("slice of cheese", 1),
-        new Ingredient("slice of bread", 1),
-        new Ingredient("butter", 1)
-      ]),
-
-  ];
 
   getRecipes() {
     return this.immutableRecipes;
@@ -84,3 +61,27 @@ export class RecipeService {
 //use this to add ingredients to shopping list
 //use this to edit recipe (editing recipe should also edit the shopping list)
 //use this to delete recipe (deleting receipe should also delete items from shopping list)
+
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     1,
+  //     "Beans on Toast",
+  //     "A tin of baked beans on a slice of toast",
+  //     "https://thumbs.dreamstime.com/b/beans-toast-9341313.jpg",
+  //     [
+  //       new Ingredient("tin of baked beans", 1),
+  //       new Ingredient("slice of bread", 1),
+  //       new Ingredient("butter", 1)
+  //     ]),
+  //   new Recipe(
+  //     2,
+  //     "Cheese on Toast",
+  //     "A slice of cheese on a slice of toast",
+  //     "https://thumbs.dreamstime.com/b/cheese-toast-9705535.jpg",
+  //     [
+  //       new Ingredient("slice of cheese", 1),
+  //       new Ingredient("slice of bread", 1),
+  //       new Ingredient("butter", 1)
+  //     ]),
+
+  // ];
--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../services/recipe.service";
 
@@ -18,7 +19,13 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-1d2ed-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+    this.http
+      .get<Recipe[]>('https://ng-course-recipe-book-1d2ed-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
