b39eab89ce6567e2a2761c68bd8825981ea68dbb
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2023-02-09 16:36:24 +0000

    284 - Resolving data before loading

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../services/recipe.service";
 
@@ -19,15 +19,15 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>('https://ng-course-recipe-book-1d2ed-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
         });
-      }))
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-      });
+      }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
+        }))
   }
 }
