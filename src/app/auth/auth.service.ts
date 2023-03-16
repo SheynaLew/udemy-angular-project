51b4e2868b4f5ff32cbecf8b8251acd32bc79832
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2023-03-16 16:55:44 +0000

    301 - adding the token to ourgoing requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Subject, throwError } from "rxjs";
+import { BehaviorSubject, throwError } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
 import { environment } from "src/environments/environment.prod";
 import { User } from "./user.model";
@@ -17,7 +17,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
   key = environment.authApiKey;
 
   constructor(private http: HttpClient) { }
