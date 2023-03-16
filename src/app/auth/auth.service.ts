941b4da76f1986658c9b039c0fab318b7d194c38
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2023-03-16 15:52:44 +0000

    299 - creating and storing the user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 import { environment } from "src/environments/environment.prod";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   idToken: string,
@@ -15,6 +16,8 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+
+  user = new Subject<User>();
 
   constructor(private http: HttpClient) { }
 
@@ -29,7 +32,13 @@ export class AuthService {
         returnSecureToken: true
       }
     )
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication(
+          resData.email,
+          resData.localId,
+          resData.idToken,
+          +resData.expiresIn)
+      }));
   }
 
   logIn(email: string, password: string) {
@@ -41,7 +50,28 @@ export class AuthService {
         returnSecureToken: true
       }
     )
-      .pipe(catchError(this.handleError));
+      .pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication(
+          resData.email,
+          resData.localId,
+          resData.idToken,
+          +resData.expiresIn)
+      })
+      );
+  }
+
+  private handleAuthentication(email: string, userId: string, idToken: string, expiresIn: number) {
+    const expirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
+    );
+
+    const user = new User(
+      email,
+      userId,
+      idToken,
+      expirationDate
+    );
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
