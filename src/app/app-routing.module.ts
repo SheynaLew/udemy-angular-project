fbb599fd7445a82e57b574d93fc53175f3760d3e
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2023-03-30 15:05:32 +0100

    306 - Adding an auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { NoRecipeComponent } from "./recipes/no-recipe/no-recipe.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
@@ -11,7 +12,7 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 const appRoutes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   {
-    path: 'recipes', component: RecipesComponent, children: [
+    path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
       { path: '', component: NoRecipeComponent },
       { path: 'new', component: RecipeEditComponent }, //has to be before id so that "new" isn't read as the dynamic parameter (id)
       { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
